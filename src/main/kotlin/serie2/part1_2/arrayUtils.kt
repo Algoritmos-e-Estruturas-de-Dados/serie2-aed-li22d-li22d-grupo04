5fdd2622aaa01b9322618a95bdf773ea4989014e
miguelmbras <miguelbras6@gmail.com>
2025-05-06 19:08:35 +0100

    Primeiro exercício feito

--- src/main/kotlin/serie2/part1_2/arrayUtils.kt
+++ src/main/kotlin/serie2/part1_2/arrayUtils.kt
@@ -2,5 +2,17 @@ package serie2.part1_2
 import kotlin.random.Random
 
 fun minimum(maxHeap: Array<Int>, heapSize: Int): Int {
-    TODO("Not yet implemented")
+
+        val startIndex = heapSize / 2    // Índice onde começam as folhas
+
+        var min = maxHeap[startIndex]  // Começa com o primeiro elemento das folhas
+
+        // Percorre apenas as folhas para encontrar o menor
+        for (i in startIndex + 1 until heapSize) {
+            if (maxHeap[i] < min) {
+                min = maxHeap[i]
+            }
+        }
+    return min
 }
+
